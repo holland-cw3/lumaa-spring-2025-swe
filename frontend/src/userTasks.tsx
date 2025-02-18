d76dbdef30514ed37f6fe14bac975450428b0630
Caleb H <hollandcw3@gmail.com>
2025-02-18 11:58:12 -0500

    view tasks and logout

--- frontend/src/userTasks.tsx
+++ frontend/src/userTasks.tsx
@@ -1,25 +1,39 @@
-import {useNavigate} from 'react-router-dom';
-import { useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
+interface Task {
+  id: number;          
+  title: string;
+  description: string;
+  iscomplete: boolean;
+}
 
-async function load() {
-   const token = localStorage.getItem('token')
+function logout(){
+  localStorage.setItem('token', '');
+  alert('You Have Logged Out, Redirecting to Login...')
+  window.location.href='/login'
+
+}
+
+async function load(setTasks: (tasks: Task[]) => void) {
+  const token = localStorage.getItem("token");
 
   try {
     const response = await fetch("http://localhost:5000/tasks", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${token}`
-      }
+        Authorization: `Bearer ${token}`,
+      },
     });
 
     if (response.ok) {
-      const res = await response.json();
-      console.log(res)
-    
+      const data = await response.json();
+      console.log(data)
+      setTasks(data);
+      return;
     } else {
-      alert("Login Failed: Username or Password is incorrect")
+      alert("Login Failed: Username or Password is incorrect");
     }
   } catch (error) {
     console.error("Error submitting data:", error);
@@ -27,25 +41,50 @@ async function load() {
 }
 
 export default function UserTasks() {
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   const navigate = useNavigate();
 
-
   useEffect(() => {
-    if (!localStorage.getItem('token')) {
-      navigate('/login');
+    if (!localStorage.getItem("token")) {
+      navigate("/login");
     }
   }, [navigate]);
 
-  load();
-
-
+  useEffect(() => {
+    load(setTasks);
+  }, []);
 
   return (
     <div>
-      {localStorage.getItem('token')}
+      <div className='header'>
+        <button onClick={logout}>
+            logout
+        </button>
+      </div>
       <table>
-
+        <thead>
+          <tr>
+            <th>
+              Title
+            </th>
+            <th>
+              Description
+            </th>
+            <th>
+              isComplete
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((t) => (
+            <tr key={t.id}>
+              <td>{t.title}</td>
+              <td>{t.description}</td>
+              <td>{t.iscomplete}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
