3e790f8603c421bec24b988a470115bd2ed4636f
Caleb H <hollandcw3@gmail.com>
2025-02-17 20:22:09 -0500

    connected to frontend

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,18 @@
 const express = require("express");
-const portNumber = 5000;
 const app = express();
-const bodyParser = require("body-parser");
+const cors = require("cors"); 
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());  // This middleware parses JSON body data
 app.set("view engine", "ejs");
 
-app.listen(portNumber, (err) => {
+app.use(cors());
+
+app.listen(3000, (err) => {
   if (err) {
     console.log("Error: Server failed. As lawyers say, you've been served");
   }
   console.log(
-    `Web server started and running at http://localhost:${portNumber}`
+    `Web server started and running at http://localhost:${3000}`
   );
   process.stdin.setEncoding("utf8");
   process.stdout.write("Type stop to shutdown the server: ");
@@ -35,6 +36,21 @@ app.get("/", (req, res) => {
   res.send("hello world!");
 });
 
-// app.post("/", (req, res) => {
+// GET /tasks – Retrieve a list of tasks (optionally filtered by user).
+app.get("/tasks", (req, res) => {
+  res.send("Tasks");
+});
+// POST /tasks – Create a new task.
+app.post("/tasks", (req, res) => {
+  const data = req.body;
+  console.log(data);
+  res.json(data)
+});
+// PUT /tasks/:id – Update a task (e.g., mark as complete, edit text).
+app.put("/tasks:id", (req, res) => {
 
-// });
+});
+// DELETE /tasks/:id – Delete a task.
+app.delete("/tasks:id", (req, res) => {
+
+});
